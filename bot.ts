86ccaa80e9940a02000ec30e14dcbb9cdb5da521
Maxemilian Grönblom <maxemiliang@gmail.com>
2020-05-15 15:21:03 +0300

    Made more logical method names and commented code

--- bot.ts
+++ bot.ts
@@ -22,13 +22,19 @@ client.on('message', (channel, tags, message, self) => {
 	const command = message.trim().split(' ')[0];
 	const arg = message.trim().split(' ').length > 1 ? message.split(' ')[1] : '';
 	if (command.toLowerCase() === '!ffotd') {
-		if (arg == '') getRandomFact(channel, sendMessage);
+		if (arg == '') getRandomFact(channel, sendFactLine);
 		// TODO: implement a "direct" fact caller which reads a direct fact.
+		// TODO: Implement a pubdev search function
 	}
 });
 
-const sendMessage = (channel: string, line: string) => {
+/**
+ * Sends a preformatted message to a channel
+ * @param channel channel to send message to.
+ * @param line Text to be inserted into message.
+ */
+const sendFactLine = (channel: string, line: string) => {
 	client
 		.say(channel, `Flutter fact of the day: ${line} #ffotd`)
 		.catch(console.error);
-};
+}; // Have to be done with a callback this way as fs.read is async
--- facts.ts
+++ facts.ts
@@ -1,10 +1,15 @@
 import * as fs from 'fs';
 
+/**
+ * Reads a txt file and selects a random line which is sent to the callback
+ * @param channel Channel to be passed on to the callback
+ * @param callback The callback which will be called once a random line is selected
+ */
 export const getRandomFact = async (
 	channel: string,
 	callback: (channel: string, line: string) => void
 ) => {
-	await fs.readFile('facts.txt', (err, data) => {
+	fs.readFile('facts.txt', (err, data) => {
 		if (err) throw err;
 		const lines = data.toString().split('\n');
 		const line = lines[Math.floor(Math.random() * lines.length)];
